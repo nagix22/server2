972e9b7c3ab47c4d5231cdfe41b85c677fd0e570
nagix22 <104242905+nagix22@users.noreply.github.com>
2022-04-23 11:41:15 +0800

    initial commit

---
+++ Server-remote/Server-remote/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_remote
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form2(int.Parse(textBox1.Text)).Show();
+            button1.Enabled = false;
+        }
+    }
+}
